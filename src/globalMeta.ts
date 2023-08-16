3f7aa5e4ff581386780959136072778c679c0f06
Dan Jordan <djordandrums@gmail.com>
2023-08-16 16:52:54 +0100

    create global metadata

---
+++ src/globalMeta.ts
@@ -0,0 +1,9 @@
+const globalMeta = {
+    siteName: "Derby Yamaha Music School",
+    siteUrl: "https://derbyyms.co.uk",
+    siteLogo: "https://derbyyms.co.uk/images/logo.png",
+    email: "info@derbyyms.co.uk",
+    description: "Professional Music Tuition In Derby. Based in Pride Park, Derby, our school has delivered Yamaha's world renowned education system for over 30 years."
+} as const;
+
+export default globalMeta;
