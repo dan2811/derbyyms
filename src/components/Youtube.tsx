5dd4c805e18c234b88e96ff864b4d88cad055e97
Dan Jordan <djordandrums@gmail.com>
2023-08-23 10:32:43 +0100

    youtube component

---
+++ src/components/Youtube.tsx
@@ -0,0 +1,22 @@
+interface YoutubeProps {
+  embedId: string | null;
+}
+
+const Youtube = ({ embedId }: YoutubeProps) => {
+  if (!embedId) return null;
+  return (
+    <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
+};
+
+export default Youtube;
