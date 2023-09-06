9c631d0a8449e7cdd38e70a35f8382412022ac94
Dan Jordan <djordandrums@gmail.com>
2023-09-06 14:50:26 +0100

    improve time helpers

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,7 +1,16 @@
 export const parseMinsPastMidnight = (minsPastMidnight: number): string => {
+    const date = new Date();
     const hours = Math.floor(minsPastMidnight / 60);
-    const mins = minsPastMidnight % 60;
-    const hoursStr = hours.toString().padStart(2, '0');
-    const minsStr = mins.toString().padStart(2, '0');
-    return `${hoursStr}:${minsStr}`;
+    const mins = Math.floor(minsPastMidnight % 60);
+    date.setHours(hours, mins);
+    return date.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
+};
+
+// Returns minsPastMidnight when passed a timeString e.g "12:03"
+export const getMinsPastMidnight = (timeString: string): number => {
+    const [hours, minutes] = timeString.split(":");
+    if (!hours || !minutes) {
+        throw new Error("Could not retrieve hours and minutes from time");
+    }
+    return (parseInt(hours) * 60) + parseInt(minutes);
 };
